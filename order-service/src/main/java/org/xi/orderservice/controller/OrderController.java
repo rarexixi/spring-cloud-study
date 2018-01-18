ecfde3ec8df1f3b41a110cca73ab05149d0bd9ab
xi <rarexixi@gmail.com>
2018-01-18 18:06:31 +0800

    远程配置测试

--- order-service/src/main/java/org/xi/orderservice/controller/OrderController.java
+++ order-service/src/main/java/org/xi/orderservice/controller/OrderController.java
@@ -1,6 +1,8 @@
 package org.xi.orderservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,7 @@ import org.xi.vo.user.UserVo;
 
 @RequestMapping("/order")
 @RestController
+@RefreshScope
 public class OrderController {
 
     @Autowired
@@ -19,6 +22,14 @@ public class OrderController {
 
     @Autowired
     UserService userService;
+
+    @Value("${order.sessionId}")
+    String sessionId;
+
+    @RequestMapping(value = "/hi")
+    public String hi() {
+        return sessionId;
+    }
 
     @RequestMapping("/getById/{id}")
     public Result<OrderVo> getById(@PathVariable("id") Integer id, String sessionId) {
