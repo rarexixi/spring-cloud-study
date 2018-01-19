48b1c5b556b494785d7249e14a94ba26c3286f6c
xi <rarexixi@gmail.com>
2018-01-19 09:54:13 +0800

    删除注解

--- order-service/src/main/java/org/xi/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/org/xi/orderservice/OrderServiceApplication.java
@@ -3,7 +3,6 @@ package org.xi.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,7 +11,6 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 @Configuration
 @EnableCircuitBreaker
-@EnableDiscoveryClient
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
