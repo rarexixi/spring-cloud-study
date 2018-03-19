f154cd6207115b2be22765638bc7e0919898f792
xi <rarexixi@gmail.com>
2018-03-19 10:43:08 +0800

    添加dashboard

---
+++ dashboard-hystrix/src/main/java/org/xi/dashboard/hystrix/HystrixDashboardApplication.java
@@ -0,0 +1,15 @@
+package org.xi.dashboard.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@EnableHystrixDashboard
+@EnableTurbine
+@SpringBootApplication
+public class HystrixDashboardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+}
